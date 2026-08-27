1db10af3136e838b19ef5e67d721ab6a984a96a4
Ben <hostellerie.org@gmail.com>
2026-08-27 07:18:52 +0200

    Harden overlay group editor and restore deletion

--- admin/overlay_group_edit.php
+++ admin/overlay_group_edit.php
@@ -2,36 +2,10 @@
 
 /* Reminder: always indent with 4 spaces (no tabs). */
 // +---------------------------------------------------------------------------+
-// | Maps Plugin 1.2.2                                                         |
+// | Maps Plugin 1.5.7                                                         |
 // +---------------------------------------------------------------------------+
 // | overlay_group_edit.php                                                    |
 // +---------------------------------------------------------------------------+
-// | Copyright (C) 2012 by the following authors:                              |
-// |                                                                           |
-// | Authors: ::Ben  ben AT geeklog DOT fr                                     |
-// +---------------------------------------------------------------------------+
-// | Created with the Geeklog Plugin Toolkit.                                  |
-// +---------------------------------------------------------------------------+
-// |                                                                           |
-// | This program is free software; you can redistribute it and/or             |
-// | modify it under the terms of the GNU General Public License               |
-// | as published by the Free Software Foundation; either version 2            |
-// | of the License, or (at your option) any later version.                    |
-// |                                                                           |
-// | This program is distributed in the hope that it will be useful,           |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of            |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
-// | GNU General Public License for more details.                              |
-// |                                                                           |
-// | You should have received a copy of the GNU General Public License         |
-// | along with this program; if not, write to the Free Software Foundation,   |
-// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           |
-// |                                                                           |
-// +---------------------------------------------------------------------------+
-
-/**
-* @package Maps
-*/
 
 require_once '../../../lib-common.php';
 require_once '../../auth.inc.php';
@@ -40,160 +14,157 @@ MAPS_getheadercode();
 
 $display = '';
 
-// Ensure user even has the rights to access this page
-if (! SEC_hasRights('maps.admin')) {
+if (!SEC_hasRights('maps.admin')) {
     $display .= MAPS_compatSiteHeader('menu', $MESSAGE[30])
-             . COM_showMessageText($MESSAGE[29], $MESSAGE[30])
-             . MAPS_compatSiteFooter();
-
-    // Log attempt to access.log
+        . COM_showMessageText($MESSAGE[29], $MESSAGE[30])
+        . MAPS_compatSiteFooter();
     COM_accessLog("User {$_USER['username']} tried to illegally access the Maps plugin administration screen.");
-
     MAPS_compatOutput($display);
     exit;
 }
 
+function MAPS_getGroupOverlayForm($group = array())
+{
+    global $_CONF, $LANG_MAPS_1;
 
-// Incoming variable filter
-$vars = array('mode'         => 'alpha',
-              'o_group_id'   => 'alpha',
-              'o_group_name' => 'text'
-			  );
-
-MAPS_filterVars($vars, $_REQUEST);
-
-/**
- * This function creates a Group of Overlay Form
- *
- * Creates a Form for a group of overlay using the supplied defaults (if specified).
- *
- * @param array $group array of values describing a group of overlay
- * @return string HTML string of overlay form
- */
-function MAPS_getGroupOverlayForm($group = array()) {
-
-    global $_CONF, $_TABLES, $_MAPS_CONF, $LANG_MAPS_1, $LANG_configselects, $LANG_ACCESS, $_USER, $_GROUPS, $_SCRIPTS;
-    
     $groupDefaults = array('o_group_id' => 0, 'o_group_name' => '');
     $group = array_merge($groupDefaults, is_array($group) ? $group : array());
 
-    
-	$display = COM_startBlock('<h1>' . $LANG_MAPS_1['group_edit'] . ' ' . $group['o_group_name']. '</h1>');
+    $safeName = htmlspecialchars((string) $group['o_group_name'], ENT_QUOTES, 'UTF-8');
+    $display = COM_startBlock('<h1>' . $LANG_MAPS_1['group_edit'] . ' ' . $safeName . '</h1>');
 
+    $template = COM_newTemplate($_CONF['path'] . 'plugins/maps/templates');
+    $template->set_file(array('map' => 'group_overlay_form.thtml'));
+    $template->set_var('site_admin_url', $_CONF['site_admin_url']);
+    $token = SEC_createToken();
+    $template->set_var(
+        'csrf_token',
+        '<input type="hidden" name="' . CSRF_TOKEN . '" value="'
+        . htmlspecialchars($token, ENT_QUOTES, 'UTF-8') . '">'
+    );
 
-	$template = COM_newTemplate($_CONF['path'] . 'plugins/maps/templates');
-	$template->set_file(array('map' => 'group_overlay_form.thtml'));
-	
-	$template->set_var('yes', $LANG_MAPS_1['yes']);
-	$template->set_var('no', $LANG_MAPS_1['no']);
-	
-	//informations
-	$template->set_var('group_overlay_presentation', $LANG_MAPS_1['group_overlay_presentation']);
-	$template->set_var('informations', $LANG_MAPS_1['informations']);
-	$template->set_var('name_label', $LANG_MAPS_1['group_overlay_name_label']);
-	$template->set_var('name', stripslashes($group['o_group_name']));
-	$template->set_var('required_field', $LANG_MAPS_1['required_field']);
+    $template->set_var('yes', $LANG_MAPS_1['yes']);
+    $template->set_var('no', $LANG_MAPS_1['no']);
+    $template->set_var('group_overlay_presentation', $LANG_MAPS_1['group_overlay_presentation']);
+    $template->set_var('informations', $LANG_MAPS_1['informations']);
+    $template->set_var('name_label', $LANG_MAPS_1['group_overlay_name_label']);
+    $template->set_var('name', $safeName);
+    $template->set_var('required_field', $LANG_MAPS_1['required_field']);
+    $template->set_var('save_button', $LANG_MAPS_1['save_button']);
 
-	//Form validation
-	$template->set_var('save_button', $LANG_MAPS_1['save_button']);
-	
-	if ($group['o_group_id'] > 0) {
-		$template->set_var('delete_button', '<option value="delete">' . $LANG_MAPS_1['delete_button'] . '</option>');
-	} else {
-		$template->set_var('delete_button', '');
-	}
-	$template->set_var('ok_button', $LANG_MAPS_1['ok_button']);
-	if (isset($group['o_group_id'])) {
-		$template->set_var('o_group_id', '<input type="hidden" name="o_group_id" value="' . $group['o_group_id'] .'" />');
-	} else {
-		$template->set_var('o_group_id', '');
-	}
-	
-	$display .= $template->parse('output', 'map');
+    if ((int) $group['o_group_id'] > 0) {
+        $template->set_var(
+            'delete_button',
+            '<option value="delete">'
+            . htmlspecialchars($LANG_MAPS_1['delete_button'], ENT_QUOTES, 'UTF-8')
+            . '</option>'
+        );
+        $template->set_var(
+            'o_group_id',
+            '<input type="hidden" name="o_group_id" value="'
+            . (int) $group['o_group_id'] . '">'
+        );
+    } else {
+        $template->set_var('delete_button', '');
+        $template->set_var('o_group_id', '');
+    }
 
-
+    $template->set_var('ok_button', $LANG_MAPS_1['ok_button']);
+    $display .= $template->parse('output', 'map');
     $display .= COM_endBlock();
 
     return $display;
 }
 
+$requestMethod = isset($_SERVER['REQUEST_METHOD']) ? strtoupper($_SERVER['REQUEST_METHOD']) : 'GET';
+$requestData = $requestMethod === 'POST' ? $_POST : $_GET;
+$mode = isset($requestData['mode']) ? COM_applyFilter($requestData['mode']) : 'new';
+$groupId = isset($requestData['o_group_id']) ? (int) $requestData['o_group_id'] : 0;
 
-// MAIN
-$o_group_id = $_REQUEST['o_group_id'];
 $display .= MAPS_compatSiteHeader('menu', $LANG_MAPS_1['plugin_name']);
 $display .= maps_admin_menu();
 
-switch ($_REQUEST['mode']) {
-    case 'delete':
-	    //Remove group associations
-		//DB_delete($_TABLES['maps_map_overlay'], 'mo_oid', $oid);
-		//Delete group of overlays
-		//DB_delete($_TABLES['maps_overlays'], 'oid', $oid);
-		if (DB_affectedRows('') == 1) {
-			$msg = $LANG_MAPS_1['deletion_succes'];
-		} else {
-			$msg = $LANG_MAPS_1['deletion_fail'];
-		}
-		// delete complete, return to map list
-		echo COM_refresh($_CONF['site_url'] . "/admin/plugins/maps/overlays.php?mode=groups&amp;msg=$msg");
+if (in_array($mode, array('save', 'delete'), true)) {
+    if ($requestMethod !== 'POST' || !SEC_checkToken()) {
+        COM_accessLog('Rejected Maps overlay group mutation because of missing or invalid CSRF token.');
+        $display .= MAPS_message('Invalid or expired security token.', $LANG_MAPS_1['error']);
+        $mode = $groupId > 0 ? 'edit' : 'new';
+    }
+}
 
-        exit();
-        break;
+switch ($mode) {
+    case 'delete':
+        if ($groupId <= 0 || (int) DB_count($_TABLES['maps_overlays_groups'], 'o_group_id', $groupId) !== 1) {
+            $msg = $LANG_MAPS_1['deletion_fail'];
+        } else {
+            DB_query(
+                "UPDATE {$_TABLES['maps_overlays']} SET o_group=0 WHERE o_group=" . $groupId
+            );
+            DB_delete($_TABLES['maps_overlays_groups'], 'o_group_id', $groupId);
+            $msg = (int) DB_count($_TABLES['maps_overlays_groups'], 'o_group_id', $groupId) === 0
+                ? $LANG_MAPS_1['deletion_succes']
+                : $LANG_MAPS_1['deletion_fail'];
+        }
+        echo COM_refresh(
+            $_CONF['site_admin_url'] . '/plugins/maps/overlays.php?mode=groups&msg=' . urlencode($msg)
+        );
+        exit;
 
     case 'save':
-        if (empty($_REQUEST['o_group_name']) ) {
-            $display .= COM_startBlock($LANG_MAPS_1['error'],'','blockheader-message.thtml');
+        $post = is_array($_POST) ? $_POST : array();
+        $groupName = trim(isset($post['o_group_name']) ? (string) $post['o_group_name'] : '');
+
+        if ($groupName === '') {
+            $display .= COM_startBlock($LANG_MAPS_1['error'], '', 'blockheader-message.thtml');
             $display .= $LANG_MAPS_1['missing_field'];
             $display .= COM_endBlock('blockfooter-message.thtml');
-            $display .= MAPS_getGroupOverlayForm($_REQUEST);
+            $display .= MAPS_getGroupOverlayForm(array_merge($post, array('o_group_id' => $groupId)));
             break;
         }
-		$o_group_name = $_REQUEST['o_group_name'];
-        $sql = "o_group_name = '{$o_group_name}'";
-		
-		if (!empty($_REQUEST['o_group_id'])) { 
-		    //edit mode
-            $sql = "UPDATE {$_TABLES['maps_overlays_groups']} SET $sql "
-                 . "WHERE o_group_id = {$o_group_id}";
-        } else {
-		    $sql = "INSERT INTO {$_TABLES['maps_overlays_groups']} SET $sql ";
-		}
-		
-        DB_query($sql);
 
-        if (DB_error()) {
-            $msg = $LANG_MAPS_1['save_fail'];
+        $escapedName = MAPS_dbEscape($groupName);
+        if ($groupId > 0) {
+            if ((int) DB_count($_TABLES['maps_overlays_groups'], 'o_group_id', $groupId) !== 1) {
+                $display .= MAPS_message($LANG_MAPS_1['save_fail'], $LANG_MAPS_1['error']);
+                $display .= MAPS_getGroupOverlayForm(array_merge($post, array('o_group_id' => $groupId)));
+                break;
+            }
+            DB_query(
+                "UPDATE {$_TABLES['maps_overlays_groups']} SET o_group_name='"
+                . $escapedName . "' WHERE o_group_id=" . $groupId
+            );
         } else {
-            $msg = $LANG_MAPS_1['save_success'];
+            DB_query(
+                "INSERT INTO {$_TABLES['maps_overlays_groups']} SET o_group_name='"
+                . $escapedName . "'"
+            );
         }
-		
-        // save complete, return to overlays list
-        echo COM_refresh($_CONF['site_admin_url'] . "/plugins/maps/overlays.php?mode=groups&amp;msg=" . urlencode($msg));
-        exit();
-        break;
+
+        $msg = DB_error() ? $LANG_MAPS_1['save_fail'] : $LANG_MAPS_1['save_success'];
+        echo COM_refresh(
+            $_CONF['site_admin_url'] . '/plugins/maps/overlays.php?mode=groups&msg=' . urlencode($msg)
+        );
+        exit;
 
     case 'edit':
-        // Get the group of overlay to edit and display the form
-        if (isset($_REQUEST['o_group_id'])) {
-            $sql = "SELECT * FROM {$_TABLES['maps_overlays_groups']} WHERE o_group_id = {$_REQUEST['o_group_id']} LIMIT 1";
-            $res = DB_query($sql, 0);
-            $A = DB_fetchArray($res);
-            $display .= MAPS_getGroupOverlayForm($A);
+        if ($groupId > 0 && (int) DB_count($_TABLES['maps_overlays_groups'], 'o_group_id', $groupId) === 1) {
+            $res = DB_query(
+                "SELECT * FROM {$_TABLES['maps_overlays_groups']} WHERE o_group_id="
+                . $groupId . ' LIMIT 1'
+            );
+            $group = DB_fetchArray($res);
+            $display .= MAPS_getGroupOverlayForm($group);
         } else {
-            echo COM_refresh($_CONF['site_url']);
+            echo COM_refresh($_CONF['site_admin_url'] . '/plugins/maps/overlays.php?mode=groups');
+            exit;
         }
         break;
-	
 
     case 'new':
     default:
-        $display .= MAPS_getGroupOverlayForm($overlay);
+        $display .= MAPS_getGroupOverlayForm(array());
         break;
 }
 
 $display .= MAPS_compatSiteFooter(0);
-
-
 MAPS_compatOutput($display);
-
-?>
